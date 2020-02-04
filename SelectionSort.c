f4696436ecb2709a2378ae187d39fb8b2bc80e6a
hanchao <1035710787@qq.com>
2020-02-04 11:07:13 +0800

    选择排序

--- SelectionSort.c
+++ SelectionSort.c
@@ -2,19 +2,19 @@
 
 /*
 选择排序的实现
-未完成
 */
 
 int main() {
     int a[8] = {1, 7, 3, 9, 10, 4, 15, 2};
-    int min = a[0];
-    int temp = 0;
+    int min = 0;
+
     for (int i = 0; i < 8; i++) {
-        for (int j = 1; j < 8; j++) {
-            if (a[j] < min) {
+        for (int j = i + 1; j < 8; j++) {
+            if (a[j] < a[i]) {
+                int temp = 0;
                 temp = a[j];
-                a[j] = min;
-                min = temp;
+                a[j] = a[i];
+                a[i] = temp;
             }
         }
     }
@@ -23,6 +23,6 @@ int main() {
     for (int i = 0; i < 8; i++) {
         printf("%d ", a[i]);
     }
-
+    putchar('\n');
     return 0;
 }
